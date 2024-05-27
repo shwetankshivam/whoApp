4823c9d287aebf26da5d62f747098a2d7f7232f6
shiv <shwetankshivam@gmail.com>
2024-05-27 10:04:40 +0530

    Initial Commit

---
+++ who/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  who
+//
+//  Created by Shwetank Shivam on 27/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ who/whoApp.swift
@@ -0,0 +1,17 @@
+//
+//  whoApp.swift
+//  who
+//
+//  Created by Shwetank Shivam on 27/05/24.
+//
+
+import SwiftUI
+
+@main
+struct whoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
